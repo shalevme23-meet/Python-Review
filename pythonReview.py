1cd9340cb60218d1c05cfeff2a3249e3321a6705
smessalem <shalevmeet@gmail.com>
2022-07-24 18:27:35 +0300

    Update pythonReview.py

--- pythonReview.py
+++ pythonReview.py
@@ -1,5 +1,5 @@
-def create_youtube_video(title, description):
-	youtube_video = {"title" : title, "description" : description, "likes" : 0, "dislikes" : 0, "comments" : {}}
+def create_youtube_video(title, description, tags):
+	youtube_video = {"title" : title, "description" : description, "likes" : 0, "dislikes" : 0, "comments" : {}, "hashtag" : tags}
 	return youtube_video
 def like(vid):
 	if "likes" in vid:
@@ -12,9 +12,18 @@ def dislike(vid):
 def comment(vid, name, txt):
 	vid["comments"][name] = txt
 	return vid
+def similarity(vid1, vid2):
+	percent = 0
+	for tag1 in vid1["hashtag"]:
+		for tag2 in vid2["hashtag"]:
+			if tag1 == tag2:
+				percent += 20
+	return percent
 
-my_vid = create_youtube_video("How to go get milk (Fathers Edition)", "pls come back dad")
-print(my_vid)
-print(like(my_vid))
-print(dislike(my_vid))
-print(comment(my_vid, "xXmissing_fatherXx", "meet me in Belaruse Timmy."))
+my_vid1 = create_youtube_video("How to go get milk (Fathers Edition)", "pls come back dad", ["sad", "missingfather", "depression", "forgotthemilk", "nomilk"])
+my_vid2 = create_youtube_video("How to ditch a child (Belarus Edition)", "easy and simple, very affordable", ["notimmylife", "missingfather", "belarus", "forgotthemilk", "nomoredad"])
+print(my_vid1)
+print(like(my_vid1))
+print(dislike(my_vid1))
+print(comment(my_vid1, "xXmissing_fatherXx", "meet me in Belarus Timmy."))
+print(similarity(my_vid1, my_vid2))
